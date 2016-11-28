04ad3e66f6536613ea96b8fc8b4c6f561a70f987
Hanine HAMZIOUI <hhamzioui@magic.fr>
2016-11-28 22:16:16 +0100

    Added check-apache-status.rb plugin - Ne mysql gem fallback

--- bin/check-mysql-status.rb
+++ bin/check-mysql-status.rb
@@ -4,7 +4,7 @@
 # ===
 #
 # This plugin attempts to login to mysql with provided credentials.
-# NO DEPENDENCIES (no mysql-devel and no implicit mysql-server restart)
+# NO DEPENDENCIES (no mysql-devel and thus no implicit mysql-server restart)
 # It checks whether MySQL is UP --check status
 # It checks replication delay --check replication
 # Author: Magic Online - www.magic.fr
@@ -62,70 +62,81 @@ class CheckMySQLStatus < Sensu::Plugin::Check::CLI
     [db_user, db_pass, db_socket]
   end
 
-  def run
-    db_user = credentials[0]; db_pass = credentials[1]; db_socket = credentials[2]
-    if config[:check] == 'status'
-      begin
-        cmd = "#{config[:binary]} -u #{db_user} -h #{config[:hostname]} --port #{config[:port]} \
+  # Status check
+  def status_check(db_user, db_pass, db_socket)
+    cmd = "#{config[:binary]} -u #{db_user} -h #{config[:hostname]} --port #{config[:port]} \
         --socket #{db_socket} -p\"#{db_pass.strip}\" --batch --disable-column-names -e 'show schemas;'"
-        stdout, _stderr, status = Open3.capture3(cmd)
-        if status.to_i == 0
-          ok "#{status} | #{stdout.split("\n")}"
-        else
-          critical "Error message: status: #{status}"
-        end
-      rescue => e
-        critical "Error message: status: #{status} | Exception: #{e}"
-      ensure
-        puts ''
+    begin
+      stdout, _stderr, status = Open3.capture3(cmd)
+      if status.to_i == 0
+        ok "#{status} | #{stdout.split("\n")}"
+      else
+        critical "Error message: status: #{status}"
       end
+    rescue => e
+      critical "Error message: status: #{status} | Exception: #{e}"
+    ensure
+      puts ''
+    end
+  end
+
+  def replication_check(db_user, db_pass, db_socket)
+    table = {}
+    begin
+      cmd = "#{config[:binary]} -u #{db_user} -h #{config[:hostname]} --port #{config[:port]} \
+        --socket #{db_socket} -p\"#{db_pass.strip}\"  -e 'SHOW SLAVE STATUS\\G'"
+      stdout, _stderr, status = Open3.capture3(cmd)
+      if status.to_i != 0
+        critical "Error message: status: #{status}"
+      end
+      stdout.split("\n").each do |line|
+        key = line.split(':')[0]
+        value = line.split(':')[1]
+        table[key.strip.to_s] = value.to_s unless key.include? '***'
+      end
+      dict = []
+      table.keys.to_a.each do |k|
+        %w(Slave_IO_State Slave_IO_Running Slave_SQL_Running Last_IO_Error Last_SQL_Error Seconds_Behind_Master).each do |key|
+          dict.push(k.strip.to_s) if key.strip == k.strip
+        end
+      end
+      table.each do |attribute, value|
+        puts "#{attribute} : #{value}" if config[:debug]
+        warn "couldn't detect replication status :#{dict.size}" unless dict.size == 6
+        slave_running = %w(Slave_IO_Running Slave_SQL_Running).all? do |key|
+          table[key].to_s =~ /Yes/
+        end
+        output = 'Slave not running!'
+        output += ' STATES:'
+        output += " Slave_IO_Running=#{table['Slave_IO_Running']}"
+        output += ", Slave_SQL_Running=#{table['Slave_SQL_Running']}"
+        output += ", LAST ERROR: #{table['Last_SQL_Error']}"
+        critical output unless slave_running
+        replication_delay = table['Seconds_Behind_Master'].to_i
+        message = "replication delayed by #{replication_delay}"
+        if replication_delay > config[:warn].to_i && replication_delay <= config[:crit].to_i
+          warning message
+        elsif replication_delay >= config[:crit].to_i
+          critical message
+        else
+          ok "slave running: #{slave_running}, #{message}"
+        end
+      end
+      ok 'show slave status was nil. This server is not a slave.'
+    rescue => e
+      critical "Error message: status: #{status} | Exception: #{e}"
+    end
+  end
+
+  def run
+    db_user = credentials[0]
+    db_pass = credentials[1]
+    db_socket = credentials[2]
+    if config[:check] == 'status'
+      status_check(db_user, db_pass, db_socket)
     end
     if config[:check] == 'replication'
-      table = {}
-      begin
-        cmd = "#{config[:binary]} -u #{db_user} -h #{config[:hostname]} --port #{config[:port]} \
-        --socket #{db_socket} -p\"#{db_pass.strip}\"  -e 'SHOW SLAVE STATUS\\G'"
-        stdout, _stderr, status = Open3.capture3(cmd)
-        if status.to_i != 0
-          critical "Error message: status: #{status}"
-        end
-        stdout.split("\n").each do |line|
-          key = line.split(':')[0]
-          value = line.split(':')[1]
-          table[key.strip.to_s] = value.to_s unless key.include? '***'
-        end
-        dict = []
-        table.keys.to_a.each do |k|
-          %w(Slave_IO_State Slave_IO_Running Slave_SQL_Running Last_IO_Error Last_SQL_Error Seconds_Behind_Master).each do |key|
-            dict.push(k.strip.to_s) if key.strip == k.strip
-          end
-        end
-        table.each do |attribute, value|
-          puts "#{attribute} : #{value}" if config[:debug]
-          warn "couldn't detect replication status :#{dict.size}" unless dict.size == 6
-          slave_running = %w(Slave_IO_Running Slave_SQL_Running).all? do |key|
-            table[key].to_s =~ /Yes/
-          end
-          output = 'Slave not running!'
-          output += ' STATES:'
-          output += " Slave_IO_Running=#{table['Slave_IO_Running']}"
-          output += ", Slave_SQL_Running=#{table['Slave_SQL_Running']}"
-          output += ", LAST ERROR: #{table['Last_SQL_Error']}"
-          critical output unless slave_running
-          replication_delay = table['Seconds_Behind_Master'].to_i
-          message = "replication delayed by #{replication_delay}"
-          if replication_delay > config[:warn].to_i && replication_delay <= config[:crit].to_i
-            warning message
-          elsif replication_delay >= config[:crit].to_i
-            critical message
-          else
-            ok "slave running: #{slave_running}, #{message}"
-          end
-        end
-        ok 'show slave status was nil. This server is not a slave.'
-      rescue => e
-        critical "Error message: status: #{status} | Exception: #{e}"
-      end
+      replication_check(db_user, db_pass, db_socket)
     end
     unknown 'No check type succeeded. Check your options'
   end
